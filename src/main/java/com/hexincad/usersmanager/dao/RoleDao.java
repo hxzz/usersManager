1c26a5c3c8313f6877ed29c247b7416efdbf1366
HandsomeJohnson <294129407@qq.com>
2016-10-27 10:15:01 +0800

    springmvc+hibernate环境+用户管理，配置了ehcache控制缓存

---
+++ src/main/java/com/hexincad/usersmanager/dao/RoleDao.java
@@ -0,0 +1,14 @@
+package com.hexincad.usersmanager.dao;
+
+import com.hexincad.usersmanager.entity.AcctRole;
+
+/**  
+ * 创建时间：2015-2-6 下午5:20:14  
+ * @author andy  
+ * @version 2.2  
+ * 描述： 角色Dao接口
+ */
+
+public interface RoleDao extends GenericDao<AcctRole, String>{
+
+}
