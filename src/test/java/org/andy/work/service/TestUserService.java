4a632ae2520e3ead2da07b1e5e3250a8d04b38b2
mazongli2015 <976639186@qq.com>
2016-11-03 16:25:33 +0800

    把依赖作用域为provided和test的依赖包从common-lib的pom.xml复制到usersManager的pom.xml中

--- src/test/java/org/andy/work/service/TestUserService.java
+++ src/test/java/org/andy/work/service/TestUserService.java
@@ -5,7 +5,10 @@ import java.util.UUID;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.alibaba.fastjson.JSON;
 import com.hexincad.usersmanager.entity.AcctUser;
